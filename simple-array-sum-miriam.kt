2351bf02bc282491e5113beb85537779dbeb965a
mdidomizio <miriam.didomizio@googlemail.com>
2024-12-03 22:45:13 +0100

    Simple Array Sum - Miriam

---
+++ simple-array-sum-miriam.kt
@@ -0,0 +1,36 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+fun simpleArraySum(ar: Array<Int>): Int {
+    var sum  = 0
+    for ( e in ar) {
+        sum += e
+    }
+    return sum
+}
+
+fun main(args: Array<String>) {
+    val arCount = readLine()!!.trim().toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = simpleArraySum(ar)
+
+    println(result)
+}
